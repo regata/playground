feb73efde103468a3660a0ce5ece2682f3bdf757
regata <renat.g@gmail.com>
2020-05-17 18:41:05 -0400

    basic webrtc example

--- webrtc/app/app.js
+++ webrtc/app/app.js
@@ -3,7 +3,9 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// the path is relative to where the node is launched
+app.use(express.static('app/public'));
+app.use('/adapter.js', express.static('node_modules/webrtc-adapter/out/adapter.js'));
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
